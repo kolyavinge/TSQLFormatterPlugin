6b7e79281a2d1c5056ade33bbe28c206a06bd93e
Nikolay Vinge <vn2756@gmail.com>
2019-02-13 22:39:31 +0400

    fix
    Signed-off-by: Nikolay Vinge <vn2756@gmail.com>

---
+++ TSQLFormatter/Utils/ExtMethods.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TSQLFormatter.Utils
+{
+    public static class StringExt
+    {
+        public static string[] SplitNewLine(this string str)
+        {
+            return str.Split(new string[] { Environment.NewLine }, StringSplitOptions.None);
+        }
+    }
+
+    public static class EnumerableExt
+    {
+        public static string JoinToString(this IEnumerable<string> collection, string separator)
+        {
+            return String.Join(separator, collection);
+        }
+    }
+}
